ae18cd09c693eacee99dc0f52a64084d45cc2755
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-04-08 22:39:18 +0900

    first commit

---
+++ functional-javascript-workshop/javascript/functional-javascript-workshop/02-Higher-Order-Functions.js
@@ -0,0 +1,8 @@
+function repeat(operation, num) {
+  if (num <= 0) return
+  operation()
+  repeat(operation, --num)
+}
+
+// Do not remove the line below
+module.exports = repeat
---
+++ php/file-upload/index.php
@@ -0,0 +1,32 @@
+<?php
+$uploaded = false;
+if (!empty($_FILES['uploaded_file'])) { 
+  $upload_dir = './upload_dir/';
+  $uploaded_file = $upload_dir . basename($_FILES['uploaded_file']['name']);
+  move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $uploaded_file);
+  $uploaded = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHPでファイルアップロード</title>
+</head>
+
+<body>
+  <?php if ($uploaded): ?>
+  <p><?php echo "ファイルのアップロードが完了しました。"; ?></p>
+  <?php endif ?>
+
+  <form enctype="multipart/form-data" action="index.php" method="POST">
+    <input type="hidden" name="name" value="value" />
+    アップロード: <input name="uploaded_file" type="file" />
+    <input type="submit" value="ファイル送信" />
+  </form>
+</body>
+
+</html>
