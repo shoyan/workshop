80c12f2d8569825c76babbac1330efb93c5724f1
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-05-02 16:15:18 +0900

    ajaxのサンプル

---
+++ javascript/ajax/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (empty($_SESSION['count'])) {
+  $_SESSION['count'] = 0;
+}
+$_SESSION['count']++;
+
+echo json_encode([
+  'fav_id' => $_POST['fav_id'],
+  'fav_count' => $_SESSION['count']]
+);
