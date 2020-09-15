2371416164424bbb3e2f590ea3a26020eec93d05
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-09-15 21:49:56 +0900

    前後のページネーション実装

---
+++ php/pagenation/index.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 1つ前のデータを取得する
+ * @param $id 現在のデータのid
+ */
+function getPrevData($id) {
+  // idの最小値
+  $minId = 1;
+
+  // idが最小値よりも大きい場合は前のデータを作成　
+  if ($id > 1) {
+    $id -= 1;
+    $data = [
+      'title' => '前へ',
+      'url' => "http://localhost:8080/?id=${id}"
+    ];
+    return $data;
+  }
+  return [];
+}
+
+/**
+ * 次のデータを取得する
+ * @param $id 現在のデータのid
+ */
+function getNextData($id) {
+  // idの最大値
+  $maxId = 10;
+
+  // idが最大値よりも小さい場合は次のデータを作成　
+  if ($id < $maxId) {
+    $id += 1;
+    $data = [
+      'title' => '次へ',
+      'url' => "http://localhost:8080/?id=${id}"
+    ];
+    return $data;
+  }
+  return [];
+}
+
+$id = $_GET['id'];
+$prev = getPrevData($id);
+$next = getNextData($id);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ページネーションのサンプル</title>
+</head>
+<body>
+
+<?php if (!empty($prev)): ?>
+  <a href="<?php echo $prev['url']?>"><?php echo $prev['title']?></a>
+<?php else: ?>
+前へ
+<?php endif ?>
+
+<?php if (!empty($next)): ?>
+  <a href="<?php echo $next['url']?>"><?php echo $next['title']?></a>
+<?php else: ?>
+次へ
+<?php endif ?>
+
+</body>
+</html>
