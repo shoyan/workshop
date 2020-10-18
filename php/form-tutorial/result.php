7e5a3e1bfd45c748526fffdf35978b4d88f9786a
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-10-18 10:29:33 +0900

    Add form-tutorial

---
+++ php/form-tutorial/result.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <table border="1">
+    <tr>
+      <td>$_GET</td>
+      <td><?php var_dump($_GET); ?></td>
+    </tr>
+    <tr>
+      <td>$_POST</td>
+      <td><?php var_dump($_POST); ?></td>
+    </tr>
+  </table>
+</body>
+<a href="./index.html">戻る</a>
+
+</html>
+<?php
