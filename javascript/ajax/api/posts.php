517f815e67b5e0e4344f4d7be1582b2b1c4bc389
Shohei Yamasaki <yamasaki0406@gmail.com>
2020-11-17 22:11:23 +0900

    ページング機能追加

--- javascript/ajax/api/posts.php
+++ javascript/ajax/api/posts.php
@@ -8,7 +8,48 @@ $posts = [
         'title' => 'はじめてのJavaScript',
         'content' => 'JavaScriptについての記事です。'
     ],
+    [
+        'title' => 'はじめてのRuby',
+        'content' => 'Rubyについての記事です。'
+    ],
+    [
+        'title' => 'はじめてのJava',
+        'content' => 'Javaについての記事です。'
+    ],
+    [
+        'title' => 'はじめてのNode.js',
+        'content' => 'Node.jsについての記事です。'
+    ],
+    [
+        'title' => 'はじめてのHTML',
+        'content' => 'HTMLについての記事です。'
+    ],
+    [
+        'title' => 'はじめてのCSS',
+        'content' => 'CSSについての記事です。'
+    ],
 ];
 
+// 一度に返すデータの個数
+if (!empty($_GET['offset'])) {
+    $offset = $_GET['offset'];
+} else {
+    $offset = 2;
+}
+
+// ページング
+if (!empty($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+
+if ($page == '1') {
+    $start = 0;
+} else {
+    $start = $offset * ($page - 1);
+}
+
+$data = array_slice($posts, $start, $offset);
 header("Content-Type: application/json; charset=utf-8");
-echo json_encode($posts);
+echo json_encode($data);
