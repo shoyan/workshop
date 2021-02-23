f89b478a2295a135caeba79f276cb8d6ca6d560e
Shohei Yamasaki <yamasaki0406@gmail.com>
2021-02-23 21:09:02 +0900

    validation-form step1

---
+++ php/validation-form/README.md
@@ -0,0 +1,26 @@
+# Validation form
+バリデーション処理を学習します。
+
+## 起動方法
+
+PHPのビルトインサーバーで起動します。
+
+```
+$ cd php/validation-form
+$ php -S localhost:8080
+```
+
+次のURLにアクセスします。
+
+`http://localhost:8080/step1/index.php`
+
+## Step1
+バリデーションを実装したフォームです。
+入力しない場合はエラーメッセージが表示されます。
+
+## Step2
+Step1は入力値が保持されません。入力値を保持するように改良したフォームです。
+
+
+## 補足情報
+style.cssは共通のCSSを利用しています。
---
+++ php/validation-form/step1/index.php
@@ -0,0 +1,75 @@
+<?php
+// エラーを格納する変数
+$errors = [];
+
+/**
+ * 実行結果を保持する。
+ * 未完了: false
+ * 完了: true
+ */
+$result = false;
+
+// POSTデータは$data変数にいれる
+$data = $_POST;
+
+if (!empty($data)) {
+  if (empty($data['user_name'])) {
+    $errors['user_name'] = '名前を入力してください。';
+  }
+  if (empty($data['email'])) {
+    $errors['email'] = 'メールアドレスを入力してください。';
+  }
+  if (empty($data['password'])) {
+    $errors['password'] = 'パスワードを入力してください。';
+  }
+
+  if (empty($errors)) {
+      $result = true;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ユーザー認証機能</title>
+  <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+  <h1>ユーザー登録</h1>
+  <?php if(!empty($errors)): ?> 
+    <ul class="error-box">
+    <?php foreach($errors as $error): ?> 
+      <li><?php echo $error; ?></li>
+    <?php endforeach ?> 
+    </ul>
+  <?php endif ?>
+
+  <?php if($result): ?>
+    <p class="success">処理が完了しました。</p>
+  <?php else: ?>
+    <div class="bg-example">
+      <form action="./index.php" method="post">
+        <div class="form-group">
+          <label for="exampleInputName">名前</label>
+          <input type="text" name="user_name" id="exampleInputName" placeholder="名前">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail">メールアドレス</label>
+          <input type="email" name="email" id="exampleInputEmail" placeholder="メールアドレス">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword">パスワード</label>
+          <input type="password" name="password" id="exampleInputPassword" placeholder="パスワード">
+        </div>
+        <button type="submit">登録</button>
+      </form>
+    </div>
+  <?php endif ?>
+</body>
+
+</html>
