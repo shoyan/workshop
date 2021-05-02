3be7a527f087e2aa3f1c82d42847ede57e71dd16
Shohei Yamasaki <yamasaki0406@gmail.com>
2021-05-02 11:18:25 +0900

    checkboxの数を取得する処理を追加

--- apps/todo/index.php
+++ apps/todo/index.php
@@ -20,9 +20,24 @@
     <h1>プロジェクト一覧</h1>
     <ul>
         <?php foreach($projects as $project): ?>
-            <li><a href="./project.php?project_id=<?php echo $project['project_id']?>"><?php echo $project['project_name']?></a></li>
+            <li>
+                <input type="checkbox" name="deleted_project">
+                <a href="./project.php?project_id=<?php echo $project['project_id']?>"><?php echo $project['project_name']?></a></li>
         <?php endforeach ?>
     </ul>
-    
+    <p id="project_selected">selected: <span>0</span></p>
+
+    <script>
+        // 選択数を表示する要素を取得
+        const projectSelectedElement = document.querySelector("#project_selected span");
+        // checkboxの1つ1つにイベントを設定
+        document.querySelectorAll("input[name='deleted_project']").forEach(function(checkbox) {
+            // checkboxのチェックを入れる or 外すと処理が実行される
+            checkbox.addEventListener('change', function() {
+                // checkboxのチェックされている数を更新
+                projectSelectedElement.textContent = document.querySelectorAll("input[name='deleted_project']:checked").length
+            })
+        })
+    </script>
 </body>
 </html>
