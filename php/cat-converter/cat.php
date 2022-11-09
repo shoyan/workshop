3e942a9c4a8f8c15b005084a1d039aa20cf61bd6
Shohei Yamasaki <yamasaki0406@gmail.com>
2022-11-09 22:06:46 +0900

    add cat converter

---
+++ php/cat-converter/cat.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #output {
+            color: red;
+            font-size: 32px;
+        }
+    </style>
+</head>
+<body>
+    <h1>猫語コンバーター</h1>
+    <p>入力した文字を猫語に変換します。</p>
+
+    <form action="./cat.php" method="get">
+        <input type="text" name="message" value="<?php echo $_GET['message'] ?>">
+        <button>実行</button>
+    </form>
+
+    <p id="output"><?php echo $_GET['message'] . 'ニャー！'?></p>
+
+</body>
+</html>
